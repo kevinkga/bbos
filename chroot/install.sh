b8cc339209513196c60b66617d9ac68a652b61b0
kaubeelack <kevin.aubeelack@txconnected.com>
2021-11-08 00:59:52 +0000

    Added more options to the deploy script

--- chroot/install.sh
+++ chroot/install.sh
@@ -1,5 +1,5 @@
 export DEBIAN_FRONTEND=noninteractive
-
+SRC_DIR=$PWD
 mkswap /swapfile
 swapon /swapfile
 free -h
@@ -16,6 +16,14 @@ apt -yq purge
 
 cd klipper-ubuntu
 ansible-playbook --connection=local playbook.yml
+
+cd ${SRC_DIR}; rm -rf /home/klipper/klipper_config; mv ratos-configuration /home/klipper/klipper_config
+cd ${SRC_DIR};
+mkdir -p /home/klipper/.ssh;
+cat id_rsa > /home/klipper/.ssh/id_rsa
+cat authorized_keys > /home/klipper/.ssh/authorized_keys
+chown -R klipper:klipper /home/klipper/.ssh
+chmod -R 600 /home/klipper/.ssh
 
 apt remove -yq ansible
 df -h
