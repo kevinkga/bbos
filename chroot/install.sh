7483cf641ed9edc956ad21c5aed58a15d1518dc8
kaubeelack <kevin.aubeelack@txconnected.com>
2021-11-10 15:24:52 +0000

    - Set correct permissions for klipper_config dir - Set klipper device node to use klipper as the group

--- chroot/install.sh
+++ chroot/install.sh
@@ -8,8 +8,9 @@ cd /root/chroot
 df -h
 
 apt -yq update
-#apt -yq upgrade
+apt -yq upgrade
 apt install -yq ansible linux-modules-extra-raspi
+apt -yq remove unattended-upgrades
 apt -yq autoclean
 apt -yq autoremove
 apt -yq purge
@@ -20,9 +21,11 @@ ansible-playbook --connection=local playbook.yml
 
 cd ${SRC_DIR};
 # klipper config
-rm -rf /home/klipper/klipper_config; cp -r ratos-configuration /home/klipper/klipper_config
-
+rm -rf /home/klipper/klipper_config;
+cp -r ratos-configuration /home/klipper/klipper_config
+chown -R klipper:klipper /home/klipper/klipper_config
 apt remove -yq ansible
+flash-kernel
 df -h
 apt -yq autoclean
 apt -yq autoremove
